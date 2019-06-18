2167032650f8c185a0d9596831c04e39b2e51add
Ram <kotra.ramakrishna@gmail.com>
2019-06-18 23:45:22 +0530

    partneremployee null check removed.

--- dkbs-services/DKBS.DTO/PartnerEmployeeDTO.cs
+++ dkbs-services/DKBS.DTO/PartnerEmployeeDTO.cs
@@ -9,29 +9,25 @@ namespace DKBS.DTO
         [JsonIgnore]
         public int PartnerEmployeeId { get; set; }
 
-        [Required]
         public string FirstName { get; set; }
-        [Required]
+       
         public string LastName { get; set; }
-        [Required]
+
         public string JobTitle { get; set; }
-        [Required]
         public string TelePhoneNumber { get; set; }
-        [Required]
         public string Email { get; set; }
         [Required]
         public string CrmPartnerAccountId { get; set; }
-        [Required]
         public string MailGroup { get; set; }
         [Required]
         public string PESharePointId { get; set; }
-        [Required]
+
         public bool EmailNotification { get; set; }
-        [Required]
+
         public bool SMSNotification { get; set; }
-        [Required]
+
         public string Identifier { get; set; }
-        [Required]
+
         public bool DeactivatedUser { get; set; }
     }
 }
--- dkbs-services/DKBS.DTO/PartnerEmployeeUpdateDTO.cs
+++ dkbs-services/DKBS.DTO/PartnerEmployeeUpdateDTO.cs
@@ -9,27 +9,17 @@ namespace DKBS.DTO
 {
     public class PartnerEmployeeUpdateDTO
     {
-        [Required]
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
-        [Required]
         public string JobTitle { get; set; }
-        [Required]
         public string TelePhoneNumber { get; set; }
-        [Required]
         public string Email { get; set; }
         [Required]
         public string CrmPartnerAccountId { get; set; }
-        [Required]
         public string MailGroup { get; set; }
-        [Required]
-        public Boolean EmailNotification { get; set; }
-        [Required]
-        public Boolean SMSNotification { get; set; }
-        [Required]
+        public bool EmailNotification { get; set; }
+        public bool SMSNotification { get; set; }
         public string Identifier { get; set; }
-        [Required]
         public bool DeactivatedUser { get; set; }
     }
 }
